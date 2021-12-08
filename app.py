fd7cb8f0f4e514aef01804646f8b13bca8fa74be
abhijit-pandey <git2020abhi>
2021-12-08 12:13:55 +0530

    Adding Chart File remove

--- app.py
+++ app.py
@@ -417,18 +417,21 @@ def refresh():
 
 @app.route('/chart/<eq>')
 def return_chart(eq):
-    if os.path.exists(f'templates/plots/{eq.upper()}.html'):
+    try:
+        # if os.path.exists(f'templates/plots/{eq.upper()}.html'):
+        #     return render_template('index_template.html', RECOM_TABLE='', HEADER= render_template(f'plots/{eq.upper()}.html'))
+        pltdf = get_data_date(eq,str((TODAY-datetime.timedelta(days=370)).date()))
+        fig = go.Figure(data=[go.Candlestick(x=pltdf['Date'],
+                        open=pltdf['Open'],
+                        high=pltdf['High'],
+                        low=pltdf['Low'],
+                        close=pltdf['Close'])])
+        fig.add_hline(y=pltdf.iloc[-1].Close, line_color='orange', annotation_text='Current Price')
+        fig.update_layout(xaxis_rangeslider_visible=False)
+        pio.write_html(fig,file=f'templates/plots/{eq.upper()}.html')
         return render_template('index_template.html', RECOM_TABLE='', HEADER= render_template(f'plots/{eq.upper()}.html'))
-    pltdf = get_data_date(eq,str((TODAY-datetime.timedelta(days=370)).date()))
-    fig = go.Figure(data=[go.Candlestick(x=pltdf['Date'],
-                    open=pltdf['Open'],
-                    high=pltdf['High'],
-                    low=pltdf['Low'],
-                    close=pltdf['Close'])])
-    fig.add_hline(y=pltdf.iloc[-1].Close, line_color='orange', annotation_text='Current Price')
-    fig.update_layout(xaxis_rangeslider_visible=False)
-    pio.write_html(fig,file=f'templates/plots/{eq.upper()}.html')
-    return render_template('index_template.html', RECOM_TABLE='', HEADER= render_template(f'plots/{eq.upper()}.html'))
+    finally:
+        os.remove(f'templates/plots/{eq.upper()}.html')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5002,debug=True)
 
